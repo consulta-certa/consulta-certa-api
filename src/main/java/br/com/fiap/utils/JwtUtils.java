8bfd371f008f2a1f348a83de084bccc7138acd30
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-11-06 20:07:49 -0300

    :bug: fix: resolve erros com key algorithm do smallrye-jwt-build
    Atualização de uso do algorítmo HS256 para RSA/RS256, criação de chaves por meio de openssl

--- src/main/java/br/com/fiap/utils/JwtUtils.java
+++ src/main/java/br/com/fiap/utils/JwtUtils.java
@@ -1,22 +1,40 @@
 package br.com.fiap.utils;
 
 import io.smallrye.jwt.build.Jwt;
+import io.smallrye.jwt.build.JwtClaimsBuilder;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 public class JwtUtils {
-    public static String generateToken (String id, String nome, String email, String telefone, String acompanhantes) {
-        int expiration = Integer.parseInt(System.getProperty("jwt.expiration.seconds", "21600"));
+    private static final String ISSUER;
+    private static final int EXPIRATION;
+
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("application");
+        ISSUER = bundle.getString("mp.jwt.verify.issuer");
+        EXPIRATION = Integer.parseInt(bundle.getString("jwt.expiration.seconds"));
+    }
+
+    public static String generateToken (
+        String id,
+        String nome,
+        String email,
+        String telefone,
+        String acompanhantes
+    ) {
         Map<String, Object> claims = new HashMap<>();
         claims.put("nome", nome);
         claims.put("email", email);
         claims.put("telefone", telefone);
         claims.put("acompanhantes", acompanhantes);
 
-        return Jwt.claims(claims)
+        JwtClaimsBuilder builder = Jwt.claims(claims)
                 .subject(id)
-                .issuer(System.getProperty("mp.jwt.verify.issuer", "consulta-certa-api"))
-                .expiresIn(expiration)
-                .sign();
+                .issuer(ISSUER)
+                .expiresIn(EXPIRATION);
+
+        return builder.sign();
     }
 }
